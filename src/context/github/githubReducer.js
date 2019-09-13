6466375879aebb247aedd31189364eb189361577
NiravPatel111 <48572469+NiravPatel111@users.noreply.github.com>
2019-09-13 16:54:18 -0400

    final changes made

---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,50 @@
+import {
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+  SEARCH_USERS,
+  DEFAULT_USERS
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case DEFAULT_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false
+      };
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false
+      };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false
+      };
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+};
